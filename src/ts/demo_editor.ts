2fc35ffb2a999193e54f3c46205f4f81646d10d8
John Haugeland <stonecypher@gmail.com>
2022-07-06 12:37:46 -0700

    seems to be building correctly

--- src/ts/demo_editor.ts
+++ src/ts/demo_editor.ts
@@ -2,7 +2,26 @@
 import { EditorView, basicSetup } from "codemirror"
 import { fsl }                    from './fsl';
 
-let editor = new EditorView({
-  extensions : [basicSetup, fsl()],
-  parent     : document.body
-});
+
+
+
+
+function bootstrap() {
+
+  const tgt = document.getElementById('editor_target');
+  if (tgt === null) { throw new Error('No target for editor!'); }
+
+  let editor = new EditorView({
+
+    extensions : [basicSetup, fsl()],
+    parent     : tgt
+
+  });
+
+}
+
+
+
+
+
+export { bootstrap };
